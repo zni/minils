e5e1708c244d68f6d447b31f04d615c63507e369
Matt Godshall <lifeinhex@gmail.com>
2019-07-06 21:30:01 -0400

    Toggle long-ish listing.

--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,42 @@ use std::fs;
 use std::io::ErrorKind;
 
 pub struct Config {
-    paths: Vec<String>
+    paths: Vec<String>,
+    long_listing: bool,
 }
 
 impl Config {
     pub fn new(args: &mut Vec<String>) -> Result<Config, &'static str> {
         args.remove(0);
 
-        if args.len() == 0 {
-            args.push(".".to_string());
+        let mut paths = Vec::new();
+        let mut long_listing = false;
+        let opts = args.iter();
+        for opt in opts {
+            match opt.as_str() {
+                "-l" => { long_listing = true; }
+                _ => paths.push(opt.clone()),
+            }
         }
 
-        Ok(Config { paths: args.clone() })
+        if paths.len() == 0 {
+            paths.push(String::from("."));
+        }
+
+        Ok(Config { paths, long_listing })
     }
 }
 
 pub fn run(config: &Config) -> std::io::Result<()> {
-    list_files(&config.paths)
+    list_files(&config)
 }
 
-fn list_files(files: &Vec<String>) -> std::io::Result<()> {
-    for file in files {
+fn list_files(config: &Config) -> std::io::Result<()> {
+    for file in &config.paths {
          match is_directory(&file) {
-            Ok(true) => list_directory(file)?,
-            Ok(false) => list_file(file)?,
-            Err(e) => handle_error(file, e),
+            Ok(true) => list_directory(&file, config.long_listing)?,
+            Ok(false) => list_file(&file, config.long_listing)?,
+            Err(e) => handle_error(&file, e),
         }
     }
     Ok(())
@@ -44,20 +55,38 @@ fn is_directory(file: &String) -> std::io::Result<bool> {
     Ok(attrs.is_dir())
 }
 
-fn list_directory(file: &String) -> std::io::Result<()> {
+fn list_directory(file: &String, long_listing: bool) -> std::io::Result<()> {
     println!("{}:", file);
+
     let dir_entries = fs::read_dir(file)?;
     for entry in dir_entries {
         let entry = entry?;
         let metadata = entry.metadata()?;
         let name = entry.file_name().into_string().unwrap();
-        println!("{:<name_width$} {:<width$}", name, metadata.len(), name_width=20, width=9);
+
+        if long_listing {
+            print_long_listing(&name, metadata.len());
+        } else {
+            print!("{} ", name);
+        }
     }
+
     println!("");
     Ok(())
 }
 
-fn list_file(file: &String) -> std::io::Result<()> {
-    println!("{}", file);
+fn list_file(file: &String, long_listing: bool) -> std::io::Result<()> {
+    if long_listing {
+        let attrs = fs::metadata(file)?;
+        print_long_listing(file, attrs.len());
+    } else {
+        println!("{}", file);
+    }
     Ok(())
+}
+
+fn print_long_listing(name: &String, len: u64) {
+    println!("{:<name_width$} {:>len_width$}B",
+        name, len, name_width=20, len_width=9
+    );
 }
